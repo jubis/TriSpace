430ca1914e09cf0f04149a91cdcd858b0e16eded
Matias Saarinen <matias.saarinen@aalto.fi>
2013-01-21 11:44:40 +0200

    Refactoring
    Move to new package

--- src/org/jubis/trispace/spaceobject/Drawable.java
+++
@@ -1,8 +0,0 @@
-package org.jubis.trispace.spaceobject;
-
-import org.jubis.trispace.util.Point;
-import org.newdawn.slick.Graphics;
-
-public interface Drawable {
-	public void render( Graphics g, Point onScreenPosition );
-}
